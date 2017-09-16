94206be07b1c8c71602a371f1c3d7763e1fc5133
Allen B <pingguo.ny+git@gmail.com>
2017-09-16 15:07:42 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,4 @@
 cd `mktemp -d`
 git clone https://github.com/allen-b1/youplay.git
-cd youplay
-if [ "$1" = "" ] ; then
-	read -p "What's your username? " username
-else
-	username=$1
-fi
-make USERNAME=$username && echo "Installed YouPlay into $username's account"
+username=`whoami`
+make USERNAME=username && echo "Installed YouPlay into $username's account"
