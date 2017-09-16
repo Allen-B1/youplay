8af7430882cd139356ab3c17a88f87cedaf509df
Allen B <pingguo.ny@gmail.com>
2017-09-16 12:50:15 -0400

    Add install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+cd `mktemp -d`
+git clone https://github.com/allen-b1/youplay.git
+cd youplay
+if [ "$1" = "" ] ; then
+	read -p "What's your username?" username
+else
+	username=$1
+fi
+make USERNAME=$username
